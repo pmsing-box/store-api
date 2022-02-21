10a379a1bbc981a82f140e064c1f8233e7dac725
bishoy melek <bishoymelekwadie@gmail.com>
2022-02-21 23:27:41 +0200

    api working

--- config/database.js
+++ config/database.js
@@ -4,17 +4,20 @@ module.exports = ({ env }) => {
     connection: {
       client: "postgres",
       connection: {
-        host: env("DATABASE_HOST", "127.0.0.1"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
-        user: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
-        schema: env("DATABASE_SCHEMA", "public"), // Not required
+        host: env(
+          "DATABASE_HOST",
+          "app-92b8fdf9-6102-4a00-8817-b124bc467acc-do-user-6788285-0.b.db.ondigitalocean.com"
+        ),
+        port: env.int("DATABASE_PORT", 25060),
+        database: env("DATABASE_NAME", "db"),
+        user: env("DATABASE_USERNAME", "db"),
+        password: env("DATABASE_PASSWORD", "w5HAVpAKP514ki9I"),
+        // schema: env("DATABASE_SCHEMA", "public"), // Not required
         ssl: {
           rejectUnauthorized: false,
         },
       },
     },
-    // debug: false,
+    debug: true,
   };
 };
